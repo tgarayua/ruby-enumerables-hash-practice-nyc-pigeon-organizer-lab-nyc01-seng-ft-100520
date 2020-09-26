a60c6875f3bfa3901fdb4edd4f610a4abdf81334
Thomas Garayua <tgarayua@gmail.com>
2020-09-26 18:34:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,9 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       binding.pry
+      names.each do |name|
+        
+      end
     end
     final_array
   end
