0eb2cf0681f0cc3c267a0d1e8640866f9cf7a1c2
Thomas Garayua <tgarayua@gmail.com>
2020-09-26 18:27:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(k, v), a|
+  data.each_with_object({}) do |(key, value), final_array|
  a[k] = v + 3
 end    binding.
 end
