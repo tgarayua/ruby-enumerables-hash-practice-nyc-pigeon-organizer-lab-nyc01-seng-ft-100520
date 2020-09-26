6376026d0a376bc12918eefab255de7caca141b0
Thomas Garayua <tgarayua@gmail.com>
2020-09-26 18:43:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,8 +11,9 @@ def nyc_pigeon_organizer(data)
           final_array[name] = {}
         end
         if !final_array[name][key]
-          !final_array[name][key] = {}
+          !final_array[name][key] = []
       end
+      final_array[name][key].push
     end
     final_array
   end
